8a0c8a876f162eefcd2a85cb8289e059e6265db9
yangrq <visualstudio2014@live.com>
2019-11-16 17:29:34 +0800

    reduce junk code.

--- board.hpp
+++ board.hpp
@@ -44,32 +44,7 @@ namespace yrq {
     }
     bitmap accessible_raw(int offset) {
       int x = offset % 8; int y = offset / 8;
-      bitmap r1, r2, r3, r4, obstacle(queen | arrow);
-      for (int i = x + 1; i < 8; ++i)
-        if (!obstacle[i][y]) r1[i][y] = 1;
-        else break;
-      for (int i = x - 1; i >= 0; --i)
-        if (!obstacle[i][y]) r2[i][y] = 1;
-        else break;
-      for (int i = y + 1; i < 8; ++i)
-        if (!obstacle[x][i]) r3[x][i] = 1;
-        else break;
-      for (int i = y - 1; i >= 0; --i)
-        if (!obstacle[x][i]) r4[x][i] = 1;
-        else break;
-      for (int i = x + 1, j = y + 1; i < 8 && j < 8; ++i, ++j)
-        if (!obstacle[i][j]) r1[i][j] = 1;
-        else break;
-      for (int i = x + 1, j = y - 1; i < 8 && j >= 0; ++i, --j)
-        if (!obstacle[i][j]) r2[i][j] = 1;
-        else break;
-      for (int i = x - 1, j = y + 1; i >= 0 && j < 8; --i, ++j)
-        if (!obstacle[i][j]) r3[i][j] = 1;
-        else break;
-      for (int i = x - 1, j = y - 1; i >= 0 && j >= 0; --i, --j)
-        if (!obstacle[i][j]) r4[i][j] = 1;
-        else break;
-      return r1 | r2 | r3 | r4;
+      return accessible(y, x);
     }
   private:
   };
